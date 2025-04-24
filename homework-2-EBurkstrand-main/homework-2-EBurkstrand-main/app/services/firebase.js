1f55ceb887ffbaac27e47dc323558e6b7e09ed93
EBurkstrand <167130060+EBurkstrand@users.noreply.github.com>
2025-04-24 13:28:33 -0500

    Update firebase.js

--- homework-2-EBurkstrand-main/homework-2-EBurkstrand-main/app/services/firebase.js
+++ homework-2-EBurkstrand-main/homework-2-EBurkstrand-main/app/services/firebase.js
@@ -4,13 +4,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBkL4XyMiVvMGsD3kipAbaQ8xCyiMn2Gq0',
-  authDomain: 'homework-2-b0789.firebaseapp.com',
-  projectId: 'homework-2-b0789',
-  storageBucket: 'homework-2-b0789.firebasestorage.app',
-  messagingSenderId: '549803176861',
-  appId: '1:549803176861:web:7ee2dae13f3da7e6f3b574',
-  measurementId: 'G-51CR6XG4RX',
+  
 };
 
 export default class FirebaseService extends Service {
